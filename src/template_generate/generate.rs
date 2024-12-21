d2c539c2a28b2121a83d38f766dd50430ebc5620
lyh <liaoyinghai@yymt.com>
2024-12-21 15:46:38 +0800

    submit

--- src/template_generate/generate.rs
+++ src/template_generate/generate.rs
@@ -3,9 +3,6 @@ use tera::{Context, Tera};
 pub fn generate_context() -> (Tera,Context) {
     let tera = Tera::new("templates/**/*").unwrap();
     let mut context:Context = tera::Context::new();
-    context.insert("name", "World");
-    let products = vec!["product1", "product2"];
-    context.insert("products", &products);
 
     (tera,context)
 }
