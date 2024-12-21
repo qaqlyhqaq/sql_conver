eeba7c45a8adef204081e3bf47841f8c38c3cb37
lyh <liaoyinghai@yymt.com>
2024-12-21 11:46:40 +0800

    submit

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     let tera = Tera::new("templates/**/*").unwrap();
     let mut context = tera::Context::new();
     context.insert("name", "World");
+    let products = vec!["product1", "product2"];
+    context.insert("products", &products);
 
     let rendered = tera.render("sql/hello.tem", &context).unwrap();
     println!("{}", rendered);
