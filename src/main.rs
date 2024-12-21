d6db5ff846f4e39e696de025d0be23153ca851e2
lyh <liaoyinghai@yymt.com>
2024-12-21 11:07:51 +0800

    submit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+
+
+
+
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    use tera::Tera;
+
+    // Create a new Tera instance and add a template from a string
+    let mut tera = Tera::new("templates/**/*").unwrap();
+    // tera.add_raw_template("hello", "Hello, {{ name }}!").unwrap();
+    // Prepare the context with some data
+    let mut context = tera::Context::new();
+    context.insert("name", "World");
+
+    // Render the template with the given context
+    let rendered = tera.render("sql/hello.tem", &context).unwrap();
+    println!("{}", rendered);
+    // assert_eq!(rendered, "Hello, World!");
+
+    // println!("Hello, world!");
 }
