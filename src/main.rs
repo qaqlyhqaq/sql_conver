0a05afcadc985a3e1680949fc6695d8f3aa50841
lyh <liaoyinghai@yymt.com>
2024-12-21 11:56:13 +0800

    submit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
+mod template_generate;
+
 use tera::Tera;
+use crate::template_generate::generate::generate_context;
 
 fn main() {
 
-    let tera = Tera::new("templates/**/*").unwrap();
-    let mut context = tera::Context::new();
-    context.insert("name", "World");
-    let products = vec!["product1", "product2"];
-    context.insert("products", &products);
+    let (tera,context) = generate_context();
 
     let rendered = tera.render("sql/hello.tem", &context).unwrap();
+
     println!("{}", rendered);
 }
