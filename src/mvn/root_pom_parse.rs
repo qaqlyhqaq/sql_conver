16e295aae2336fa92b00e5f0991ea4cc2dd14f0a
lyh <liaoyinghai@yymt.com>
2024-12-25 16:14:35 +0800

    submit

--- src/mvn/root_pom_parse.rs
+++ src/mvn/root_pom_parse.rs
@@ -1,4 +1,6 @@
-use quick_xml::events::Event;
+use std::any::Any;
+use std::fmt::{Debug, Formatter};
+use quick_xml::events::{BytesStart, Event};
 use std::fs::read;
 use std::io::Read;
 use std::path::PathBuf;
@@ -13,6 +15,41 @@ pub struct Pom {
     //是否具有源码文件 , 如果有,则记录路径
     src_store: Option<PathBuf>,
     sub_module: Vec<Pom>,
+}
+
+
+trait Stack<T>{
+    fn push(&mut self,item:T);
+    fn pop(&mut self,)->T;
+    fn len(&self) -> usize;
+    fn display(&self);
+}
+
+impl<T: std::fmt::Display> Stack<T> for Vec<T> {
+
+    fn push(&mut self, item: T) {
+        self.push(item);
+    }
+
+    fn pop(&mut self) -> T {
+        self.pop().unwrap()
+    }
+
+    fn len(&self) -> usize{
+        self.len()
+    }
+
+    fn display(&self){
+        self.iter().for_each(|item| println!("{}", item));
+    }
+
+}
+
+impl<T> Debug for dyn Stack<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.display();
+        return Ok(());
+    }
 }
 
 impl Pom {
@@ -42,16 +79,35 @@ impl Pom {
             sub_module: vec![],
         };
 
+         let mut  vec1 = Vec::new();
+        let stack: &mut dyn Stack<std::string::String> = &mut vec1;
+
         loop {
-            match reader.read_event() {
-                Ok(Event::Start(start_tag))
-                    if start_tag.name().0.eq("module".as_bytes()) =>  {
-                    let result = reader.read_text(start_tag.name()).unwrap();
-                    println!("text:{}", result);
+            let event = reader.read_event();
+
+            match event.clone() {
+                Ok(Event::Start(start_tag)) =>  {
+                    let string1 = String::from_utf8(start_tag.name().0.to_vec()).unwrap();
+                    println!("push:{}", string1);
+                    stack.push(string1);
                 },
+                _ => {
+                    //ignores !
+                }
+            }
+
+
+            
+            match event.clone() {
+                // Ok(Event::Start(start_tag))
+                //     if start_tag.name().0.eq("module".as_bytes()) =>  {
+                //     let result = reader.read_text(start_tag.name()).unwrap();
+                //     println!("text:{}", result);
+                // },
                 Ok(Event::Start(start_tag))
-                if start_tag.name().0.eq("project".as_bytes()) =>  {
-                    let result = reader.read_text(start_tag.name()).unwrap();
+                if start_tag.name().0.eq("artifactId".as_bytes())
+                && stack.len() == 2=>  {
+                    let result = reader.clone().read_text(start_tag.name()).unwrap();
                     pom.artifact_id = result.to_string();
                 },
                 Ok(Event::Eof)=>break,
@@ -59,6 +115,22 @@ impl Pom {
                 _ => {
                     //ignores !
                     // println!("Event:{}", event_string);
+                }
+            }
+
+
+            match event.clone() {
+                Ok(Event::End(end_tag)) =>  {
+                    let string1 = String::from_utf8(end_tag.name().0.to_vec()).unwrap();
+                    let string2 = stack.pop();
+                    if ! string1.eq(&string2) {
+                        dbg!(&stack);
+                    }
+                    assert_eq!(string1, string2);
+                    println!("pop:{}", string1);
+                }
+                _ => {
+                    //ignores !
                 }
             }
         }
