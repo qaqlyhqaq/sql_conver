e9d61d61777984f15f7e7606434d4c0391006a89
lyh <liaoyinghai@yymt.com>
2024-12-28 10:24:52 +0800

    submit

--- src/sql/generate_lombok_call.rs
+++ src/sql/generate_lombok_call.rs
@@ -5,8 +5,9 @@ use crate::capitalize_first_letter;
 use crate::sql::fetch_table_struct::fetch_table_struct;
 
 
-
-
+/*
+不指定agg 内对象方法,默认同名
+ */
 async  fn generate_lombok_get_accessors(table_name:String){
     let table = fetch_table_struct(table_name).await;
 
@@ -14,13 +15,22 @@ async  fn generate_lombok_get_accessors(table_name:String){
         .map(|col| { col.showName() })
         .collect::<Vec<_>>();
 
+    let filter_array = 
+        vec!(
+        "updateBy"
+        , "updateTime"
+        , "deleted"
+        , "createTime"
+        , "createBy"
+        , "id");
+
+    let filter_array = filter_array.iter()
+        .map(|col| { col.to_string().to_lowercase() })
+        .collect::<Vec<_>>();
+
     x.iter()
         .filter(|x1| {
-           return !( x1.contains("updateBy")
-               |x1.contains("updateTime")
-               |x1.contains("deleted")
-            ||x1.contains("createTime")
-            ||x1.contains("createBy"));
+           return ! filter_array.contains(&x1.to_lowercase().trim().to_string());
         })
     .for_each(|col| {
         let string = capitalize_first_letter(&col);
@@ -38,6 +48,7 @@ mod tests {
     #[tokio::test]
     async fn generate_lombok_call(){
         let table_name = "tb_official_resource_store".to_string();
-        generate_lombok_get_accessors(table_name).await;
+        generate_lombok_get_accessors(table_name.clone()).await;
     }
+
 }
